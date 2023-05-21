d7614d925988b84d04d07fbc7d786f527aade719
Deha Efecan Süer <dehasuer97@gmail.com>
2023-05-21 21:27:12 +0300

    api key hidden

--- src/api.js
+++ src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const searchImages =async (term) => {
   const response =  await axios.get('https://api.unsplash.com/search/photos' , {
       headers: {
-          Authorization: 'Client-ID 1THfhCMQkNbvqeOR7fQZFJvLx2E1peuLPSvspjkQe0c'
+          Authorization: 'Client-ID #enter your API Key'
       },
       params: {
         query: term,
